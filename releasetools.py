8769e5c406ea0826e9c149a3122d6346c0fcfb37
yumingyang <yumingyang@xiaomi.com>
2013-01-16 11:10:53 +0800

    add firmware version assertions
    Change-Id: If797a7d22cf68d697ab294024af19f7b9bd8bafb

--- releasetools.py
+++ releasetools.py
@@ -2,7 +2,45 @@ import common
 import edify_generator
 import copy
 
-def AddAssertions(info):
+def AddFirmwareVersionAssertions(info):
+    edify = info.script
+    version_assert = """
+################################################################################################################
+unmount("/system");
+mount("ext4", "EMMC", "/dev/block/mmcblk0p15", "/system");
+if file_getprop("/system/build.prop","ro.build.display.id") != "U9508V100R001CHNC00B542"
+then
+    ui_print("");
+    ui_print("");
+    ui_print("");
+    ui_print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    ui_print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    ui_print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    ui_print("");
+    ui_print("The current firmware version is: " + file_getprop("/system/build.prop","ro.build.display.id"));
+    ui_print("");
+    ui_print("Please flash EMUI rom which version is CHNC00B542 firstly!!!");
+    ui_print("");
+    ui_print("You can down it from 'http://cn.ui.vmall.com/emotiondownload.php'");
+    ui_print("");
+    ui_print("Then you can flash MIUI rom");
+    ui_print("");
+    ui_print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    ui_print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    ui_print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+    ui_print("");
+    ui_print("");
+    ui_print("");
+    abort();
+endif;
+################################################################################################################
+"""
+    for i in xrange(len(edify.script)):
+        if ");" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
+            edify.script[i] = edify.script[i].replace(");", ");" + version_assert)
+            return
+
+def AddDeviceAssertions(info):
     edify = info.script
     for i in xrange(len(edify.script)):
         if ");" in edify.script[i] and ("ro.product.device" in edify.script[i] or "ro.build.product" in edify.script[i]):
@@ -29,12 +67,13 @@ def RemoveCache(info):
     return
 
 def FullOTA_InstallEnd(info):
-    AddAssertions(info)
+    AddDeviceAssertions(info)
+    AddFirmwareVersionAssertions(info)
     PerpareData(info)
     RemoveCache(info)
     return
 
 def IncrementalOTA_InstallEnd(info):
-    AddAssertions(info)
-    #PerpareData(info)
+    AddDeviceAssertions(info)
     return
+
